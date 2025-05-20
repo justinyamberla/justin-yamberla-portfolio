4efd031f7cf70b0a58e911ee805f26e5a00c9353
Justin_Yamberla <justin.yamberla@epn.edu.ec>
2025-05-20 12:59:29 -0500

    Changes

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import {ModeToggle} from "@/components/ui/mode-toggle";
 
 export default function Home() {
   return (
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+      <main className="flex flex-col items-center px-4">
         <ModeToggle />
       </main>
   );
