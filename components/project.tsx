fe5ef340868d78aa634dea24840d614e280dc351
Justin_Yamberla <justin.yamberla@epn.edu.ec>
2025-05-21 12:04:39 -0500

    Changes
    bg-white/80 for blur on header

--- components/project.tsx
+++ components/project.tsx
@@ -7,35 +7,35 @@ import { motion, useScroll, useTransform } from "framer-motion";
 
 type ProjectProps = (typeof projectsData)[number];
 
-export default function Project({
-                                    title,
-                                    description,
-                                    tags,
-                                    imageUrl,
-                                }: ProjectProps) {
-    /*const ref = useRef<HTMLDivElement>(null);
+export default function Project({ title, description, tags, imageUrl }: ProjectProps) {
+
+    const ref = useRef<HTMLDivElement>(null);
     const { scrollYProgress } = useScroll({
         target: ref,
         offset: ["0 1", "1.33 1"],
     });
-    const scaleProgess = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
-    const opacityProgess = useTransform(scrollYProgress, [0, 1], [0.6, 1]);*/
+    const scaleProgress = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
+    const opacityProgress = useTransform(scrollYProgress,  [0, 1], [0.6, 1]);
     
     return (
-        <div
-
+        <motion.div
+            ref={ref}
+            style={{
+                scale: scaleProgress,
+                opacity: opacityProgress
+            }}
             className="group mb-3 sm:mb-8 last:mb-0"
         >
-            <section className="bg-slate-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem] hover:bg-slate-200 transition sm:group-even:pl-8 dark:text-white dark:bg-slate-800 dark:hover:bg-slate-700">
+            <section className="bg-slate-300 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[22rem] hover:bg-slate-100 transition sm:group-even:pl-8 dark:text-white dark:bg-slate-800 dark:hover:bg-slate-700">
                 <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]">
                     <h3 className="text-2xl font-semibold">{title}</h3>
-                    <p className="mt-2 leading-relaxed text-gray-700 dark:text-white/70">
+                    <p className="mt-2 leading-relaxed text-slate-800 dark:text-slate-100/70">
                         {description}
                     </p>
                     <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
                         {tags.map((tag, index) => (
                             <li
-                                className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full dark:text-white/70"
+                                className="bg-slate-950/70 dark:bg-slate-950/80 px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full dark:text-white/70"
                                 key={index}
                             >
                                 {tag}
@@ -48,9 +48,19 @@ export default function Project({
                     src={imageUrl}
                     alt="Project I worked on"
                     quality={95}
-                    className="absolute hidden sm:block top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl transition group-hover:scale-[1.04] group-hover:-translate-x-3 group-hover:translate-y-3 group-hover:-rotate-2 group-even:group-hover:translate-x-3 group-even:group-hover:translate-y-3 group-even:group-hover:rotate-2 group-even:right-[initial] group-even:-left-40"
+                    className="absolute hidden sm:block top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl transition
+                    group-hover:scale-[1.04]
+                    group-hover:-translate-x-3
+                    group-hover:translate-y-3
+                    group-hover:-rotate-2
+                    group-even:group-hover:translate-x-3
+                    group-even:group-hover:translate-y-3
+                    group-even:group-hover:rotate-2
+                    group-even:right-[initial]
+                    group-even:-left-40
+                    "
                 />
             </section>
-        </div>
+        </motion.div>
     );
 }
