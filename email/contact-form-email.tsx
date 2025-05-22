fc0ec2c9b12401a7c073e31a4253fbd974a20f95
Justin_Yamberla <justin.yamberla@epn.edu.ec>
2025-05-22 17:45:12 -0500

    Fix

--- email/contact-form-email.tsx
+++ email/contact-form-email.tsx
@@ -1,31 +1,32 @@
 import React from "react";
-import { Html, Body, Head, Heading, Hr, Container, Preview, Section, Text } from "@react-email/components";
-import { Tailwind } from "@react-email/tailwind";
+import {Html, Body, Head, Heading, Hr, Container, Preview, Section, Text} from "@react-email/components";
+import {Tailwind} from "@react-email/tailwind";
 
 type ContactFormEmailProps = {
     message: string;
     senderEmail: string;
 }
 
-export default function ContactFormEmail({ message, senderEmail }: ContactFormEmailProps) {
+export default function ContactFormEmail({message, senderEmail}: ContactFormEmailProps) {
     return <Html>
-        <Head>
-            <title>Contact Form Email</title>
-            <Preview>New message from your portfolio site</Preview>
-            <Tailwind />
-        </Head>
-        <Body className="bg-white text-black">
-            <Container className="p-4">
-                <Section className="mb-4">
-                    <Heading className="text-2xl font-bold">You received the following message from the contact form</Heading>
-                </Section>
-                <Hr />
-                <Section className="mt-4">
-                    <Text className="text-lg">{message}</Text>
-                    <Hr />
-                    <Text className="text-lg">The sender email is: {senderEmail}</Text>
-                </Section>
-            </Container>
-        </Body>
+        <Head/>
+        <title>Contact Form Email</title>
+        <Preview>New message from your portfolio site</Preview>
+        <Tailwind>
+            <Body className="bg-white text-black">
+                <Container className="p-4">
+                    <Section className="mb-4">
+                        <Heading className="text-2xl font-bold">You received the following message from the contact
+                            form</Heading>
+                    </Section>
+                    <Hr/>
+                    <Section className="mt-4">
+                        <Text className="text-lg">{message}</Text>
+                        <Hr/>
+                        <Text className="text-lg">The sender email is: {senderEmail}</Text>
+                    </Section>
+                </Container>
+            </Body>
+        </Tailwind>
     </Html>
 }
