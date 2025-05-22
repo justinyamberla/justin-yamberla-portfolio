04a69476560b878dc0b85e1c51ad2a646d1e4a75
Justin_Yamberla <justin.yamberla@epn.edu.ec>
2025-05-22 17:48:51 -0500

    Fix
    Footer text color

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Footer() {
     return (
-        <footer className="mb-10 px-4 text-center text-slate-200">
+        <footer className="mb-10 px-4 text-center text-slate-900 dark:text-slate-200">
             <small className="mb-2 block text-xs">
                 &copy; 2025 Justin Yamberla. Todos los derechos reservados.
             </small>
